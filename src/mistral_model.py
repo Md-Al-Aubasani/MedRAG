d80837a3338a836be0aec41c373b5ff6d198ad61
Mohammed Al Aubasani <mohammed.al.aubasani@gmail.com>
2024-05-11 21:54:37 +0800

    Create mistral_model.py

---
+++ src/mistral_model.py
@@ -0,0 +1,81 @@
+# fix numpy in colab
+import numpy
+from IPython.display import clear_output
+
+# fix triton in colab
+!export LC_ALL="en_US.UTF-8"
+!export LD_LIBRARY_PATH="/usr/lib64-nvidia"
+!export LIBRARY_PATH="/usr/local/cuda/lib64/stubs"
+!ldconfig /usr/lib64-nvidia
+
+!git clone https://github.com/dvmazur/mixtral-offloading.git --quiet
+!cd mixtral-offloading && pip install -q -r requirements.txt
+!huggingface-cli download lavawolfiee/Mixtral-8x7B-Instruct-v0.1-offloading-demo --quiet --local-dir Mixtral-8x7B-Instruct-v0.1-offloading-demo
+
+clear_output()
+
+import sys
+
+sys.path.append("mixtral-offloading")
+import torch
+from torch.nn import functional as F
+from hqq.core.quantize import BaseQuantizeConfig
+from huggingface_hub import snapshot_download
+from IPython.display import clear_output
+from tqdm.auto import trange
+from transformers import AutoConfig, AutoTokenizer
+from transformers.utils import logging as hf_logging
+
+from src.build_model import OffloadConfig, QuantConfig, build_model
+
+
+def get_mistral():
+  model_name = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+  quantized_model_name = "lavawolfiee/Mixtral-8x7B-Instruct-v0.1-offloading-demo"
+  state_path = "Mixtral-8x7B-Instruct-v0.1-offloading-demo"
+  
+  config = AutoConfig.from_pretrained(quantized_model_name)
+  
+  device = torch.device("cuda:0")
+  
+  ##### Change this to 5 if you have only 12 GB of GPU VRAM #####
+  offload_per_layer = 4
+  # offload_per_layer = 5
+  ###############################################################
+  
+  num_experts = config.num_local_experts
+  
+  offload_config = OffloadConfig(
+      main_size=config.num_hidden_layers * (num_experts - offload_per_layer),
+      offload_size=config.num_hidden_layers * offload_per_layer,
+      buffer_size=4,
+      offload_per_layer=offload_per_layer,
+  )
+  
+  
+  attn_config = BaseQuantizeConfig(
+      nbits=4,
+      group_size=64,
+      quant_zero=True,
+      quant_scale=True,
+  )
+  attn_config["scale_quant_params"]["group_size"] = 256
+  
+  
+  ffn_config = BaseQuantizeConfig(
+      nbits=2,
+      group_size=16,
+      quant_zero=True,
+      quant_scale=True,
+  )
+  quant_config = QuantConfig(ffn_config=ffn_config, attn_config=attn_config)
+  
+  
+  model = build_model(
+      device=device,
+      quant_config=quant_config,
+      offload_config=offload_config,
+      state_path=state_path,
+  )
+
+  return model
